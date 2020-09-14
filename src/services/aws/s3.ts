5b177d74fa2fb9ba637131e5c49494c8ee18cb0d
Robbie Hughes <rwhughes91@gmail.com>
2020-09-13 22:36:32 -0700

    aws secret key name change

--- src/services/aws/s3.ts
+++ src/services/aws/s3.ts
@@ -3,8 +3,8 @@ import AWS from 'aws-sdk'
 export const bucket = process.env.S3_BUCKET_NAME || ''
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
 })
 
 export default s3
